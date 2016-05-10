bb2c03241760bec138f36f95b24cb4f647bb38fd
Igor <igor.pogosian@small-team.com>
2016-05-10 11:48:50 +0300

    fixed dynamic method creation

--- factory.rb
+++ factory.rb
@@ -1,12 +1,12 @@
 # Dynamic class creator factory
 class Factory
   def self.new(*attrs, &block)
-    Class.new do
+    klass = Class.new do
       attr_accessor(*attrs)
       define_method(:initialize) do |*vals|
         if vals.size != attrs.size
-          text = "Wrong number of arguments.
-                 #{vals.size} given, instead of #{attrs.size}"
+          text = "Wrong number of arguments,
+                 given #{vals.size}, expected #{attrs.size}"
           raise(ArgumentError, text)
         end
         attrs.each_with_index do |val, key|
@@ -14,8 +14,8 @@ class Factory
         end
       end
     end
+
+    klass.class_eval(&block) if block_given?
+    klass
   end
 end
-Klass = Factory.new(:name, :surname)
-obj = Klass.new('ivan', 'petrov')
-puts obj.surname
